91a9e23c04c7ac3bde30a277c29e1fe244343e5c
Rodrigo Queiroz <rodrigo.queiroz0629@gmail.com>
2026-02-11 23:19:50 -0300

    ﻿feat(ui): Adiciona setup inicial de estilos e tema

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
+      '@': resolve(__dirname, 'src')
+    }
+  }
 })
