e5e2a6eed76f0f4f12af85d3e821564ba09c4d1a
Rodrigo Queiroz <rodrigo.queiroz0629@gmail.com>
2026-02-12 22:34:19 -0300

    ﻿feat: cria a pÃ¡gina de dashboard e refatora a de cadastro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,134 +1,216 @@
 import {
-  Check,
-  EyeClosed,
-  Lock,
-  LogIn,
-  Mail,
-  UserRound,
-  UserRoundPlus
+  BriefcaseBusiness,
+  ChevronRight,
+  CircleArrowDown,
+  CircleArrowUp,
+  Plus,
+  Wallet
 } from 'lucide-react'
 import { Logo } from './components/logo'
 
 export function App() {
   return (
     <main className="min-h-dvh">
-      <div className="pt-12 max-w-md mx-auto flex flex-col items-center gap-8">
-        <Logo className="h-8" />
+      <header className="py-4 border-b border-gray-200">
+        <div className="flex items-center justify-between max-w-296 mx-auto">
+          <Logo className="h-6" />
 
-        <div className="w-full bg-white border border-gray-200 p-8 rounded-xl flex flex-col gap-8">
-          <div>
-            <h1 className="text-gray-800 text-xl font-bold text-center mb-1">
-              Criar conta
-            </h1>
+          <nav>
+            <ul className="flex items-center gap-5">
+              <li>
+                <a href="#" className="font-semibold text-sm text-brand-base">
+                  Dashboard
+                </a>
+              </li>
 
-            <p className="text-center text-gray-600">
-              Comece a controlar suas finanças ainda hoje
-            </p>
+              <li>
+                <a
+                  href="#"
+                  className="text-sm text-gray-600 hover:text-brand-base transition-colors"
+                >
+                  Transações
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="text-sm text-gray-600 hover:text-brand-base transition-colors"
+                >
+                  Categorias
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="size-9 rounded-full text-sm font-medium text-gray-800 uppercase flex items-center justify-center bg-gray-300">
+            CT
+          </div>
+        </div>
+      </header>
+
+      <div className="max-w-296 mx-auto pt-12 space-y-6">
+        <section className="grid grid-cols-3 gap-6">
+          <div className="bg-white border border-gray-200 rounded-xl p-6 flex flex-col gap-4">
+            <div className="flex items-center gap-3">
+              <Wallet className="size-5 text-purple-base" />
+              <h2 className="text-xs text-gray-500 font-medium tracking-[0.0375em] uppercase">
+                Saldo total
+              </h2>
+            </div>
+
+            <strong className="text-[1.75rem] leading-8 font-bold text-gray-800">
+              R$ 12.000,00
+            </strong>
+          </div>
+          <div className="bg-white border border-gray-200 rounded-xl p-6 flex flex-col gap-4">
+            <div className="flex items-center gap-3">
+              <CircleArrowUp className="size-5 text-brand-base" />
+              <h2 className="text-xs text-gray-500 font-medium tracking-[0.0375em] uppercase">
+                Receitas do mês
+              </h2>
+            </div>
+
+            <strong className="text-[1.75rem] leading-8 font-bold text-gray-800">
+              R$ 12.000,00
+            </strong>
+          </div>
+          <div className="bg-white border border-gray-200 rounded-xl p-6 flex flex-col gap-4">
+            <div className="flex items-center gap-3">
+              <CircleArrowDown className="size-5 text-red-base" />
+              <h2 className="text-xs text-gray-500 font-medium tracking-[0.0375em] uppercase">
+                Despesas do mês
+              </h2>
+            </div>
+
+            <strong className="text-[1.75rem] leading-8 font-bold text-gray-800">
+              R$ 12.000,00
+            </strong>
+          </div>
+        </section>
+
+        <section className="grid grid-cols-3 gap-6">
+          <div className="col-span-2">
+            <table className="w-full border border-gray-200 rounded-xl bg-white border-separate border-spacing-0">
+              <thead>
+                <tr>
+                  <th className="px-6 py-5 text-left uppercase text-xs font-medium text-gray-500 tracking-[0.0375em] border-b border-gray-200">
+                    Transações recentes
+                  </th>
+
+                  <th className="border-b border-gray-200 w-40"></th>
+
+                  <th className="text-right border-b border-gray-200 w-44">
+                    <a
+                      href="#"
+                      className="px-6 py-5 inline-flex items-center gap-1 text-sm text-medium text-brand-base hover:text-brand-dark transition-colors"
+                    >
+                      Ver todas
+                      <ChevronRight className="size-5" />
+                    </a>
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td className="px-6 py-5">
+                    <div className="flex items-center gap-4">
+                      <div className="size-10 flex items-center justify-center bg-green-light text-green-base rounded-lg">
+                        <BriefcaseBusiness className="size-4" />
+                      </div>
+
+                      <div className="flex flex-col gap-0.5">
+                        <span className="font-medium text-gray-800">
+                          Pagamento de salário
+                        </span>
+
+                        <span className="text-sm text-gray-600">01/12/26</span>
+                      </div>
+                    </div>
+                  </td>
+
+                  <td className="px-6 py-5 text-center">
+                    <span className="px-3 py-1 rounded-full bg-green-light text-sm font-medium text-green-dark">
+                      Receita
+                    </span>
+                  </td>
+                  <td className="px-6 py-5">
+                    <div className="flex items-center justify-end gap-2">
+                      <span className="text-sm font-semibold text-gray-800">
+                        + R$ 12.000,00
+                      </span>
+
+                      <CircleArrowUp className="size-4 text-brand-base" />
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+
+              <tfoot>
+                <tr>
+                  <td
+                    colSpan={3}
+                    className="text-center px-6 border-t border-gray-200"
+                  >
+                    <div className="flex justify-center py-5">
+                      <a
+                        href="#"
+                        className="inline-flex items-center gap-1 text-sm font-medium text-brand-base hover:text-brand-dark transition-colors"
+                      >
+                        <Plus className="size-5" />
+                        Nova transação
+                      </a>
+                    </div>
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
           </div>
 
-          <form action="#" className="space-y-6">
-            <div className="space-y-4">
-              <div className="flex flex-col gap-2">
-                <label
-                  htmlFor="name"
-                  className="text-gray-700 font-medium text-sm"
-                >
-                  Nome completo
-                </label>
+          <div>
+            <table className="w-full border border-gray-200 rounded-xl bg-white border-separate border-spacing-0">
+              <thead>
+                <tr>
+                  <th
+                    colSpan={2}
+                    className="px-6 py-5 text-left uppercase text-xs font-medium text-gray-500 tracking-[0.0375em] border-b border-gray-200"
+                  >
+                    Categorias
+                  </th>
 
-                <div className="border border-gray-300 bg-white rounded-lg px-3 flex items-center gap-3">
-                  <div>
-                    <UserRound className="size-4 text-gray-400" />
-                  </div>
+                  <th className="text-right border-b border-gray-200 w-22.5">
+                    <a
+                      href="#"
+                      className="px-6 py-5 inline-flex items-center gap-1 text-sm font-medium text-brand-base hover:text-brand-dark transition-colors"
+                    >
+                      Gerenciar
+                      <ChevronRight className="size-5" />
+                    </a>
+                  </th>
+                </tr>
+              </thead>
 
-                  <input
-                    type="text"
-                    id="name"
-                    className="w-full py-3.5 focus:outline-none leading-2 text-gray-800"
-                    placeholder="Seu nome completo"
-                  />
-                </div>
-              </div>
+              <tbody>
+                <tr>
+                  <td className="pl-6 py-5">
+                    <span className="px-3 py-1 rounded-full bg-blue-light text-sm font-medium text-blue-dark">
+                      Alimentação
+                    </span>
+                  </td>
 
-              <div className="flex flex-col gap-2">
-                <label
-                  htmlFor="email"
-                  className="text-gray-700 font-medium text-sm"
-                >
-                  E-mail
-                </label>
+                  <td className="px-1 py-5 text-right text-gray-600 text-sm">
+                    12 itens
+                  </td>
 
-                <div className="border border-gray-300 bg-white rounded-lg px-3 flex items-center gap-3">
-                  <div>
-                    <Mail className="size-4 text-gray-400" />
-                  </div>
-
-                  <input
-                    type="email"
-                    id="email"
-                    className="w-full py-3.5 focus:outline-none leading-2 text-gray-800"
-                    placeholder="mail@exemplo.com"
-                  />
-                </div>
-              </div>
-
-              <div className="flex flex-col gap-2">
-                <label
-                  htmlFor="password"
-                  className="text-gray-700 font-medium text-sm"
-                >
-                  Senha
-                </label>
-
-                <div className="border border-gray-300 bg-white rounded-lg px-3 flex items-center gap-3">
-                  <div>
-                    <Lock className="size-4 text-gray-400" />
-                  </div>
-
-                  <input
-                    type="password"
-                    id="password"
-                    className="w-full py-3.5 focus:outline-none leading-2 text-gray-800"
-                    placeholder="Digite sua senha"
-                  />
-
-                  <button type="button" className="hover:cursor-pointer">
-                    <EyeClosed className="size-4 text-gray-700" />
-                  </button>
-                </div>
-
-                <span className="text-xs text-gray-500 select-none">
-                  A senha deve ter no mínimo 8 caracteres.
-                </span>
-              </div>
-            </div>
-
-            <button
-              type="button"
-              className="bg-brand-base rounded-lg px-4 py-3 flex items-center justify-center gap-2 font-medium text-white w-full border border-brand-base hover:cursor-pointer hover:bg-brand-dark hover:border-brand-dark transition-colors"
-            >
-              Cadastrar
-            </button>
-
-            <div className="flex items-center gap-3 select-none">
-              <hr className="flex-1 border-gray-300" />
-              <span className="text-sm text-gray-500">ou</span>
-              <hr className="flex-1 border-gray-300" />
-            </div>
-
-            <p className="text-gray-600 text-sm text-center">
-              Ainda não tem uma conta?
-            </p>
-
-            <a
-              href="#"
-              className="bg-white rounded-lg px-4 py-3 flex items-center justify-center gap-2 font-medium text-gray-700 w-full border border-gray-300 hover:cursor-pointer hover:bg-gray-200 transition-colors"
-            >
-              <LogIn className="size-3.5" />
-              Já tem uma conta?
-            </a>
-          </form>
-        </div>
+                  <td className="pr-6 py-5 text-right font-bold text-gray-800 text-sm">
+                    R$ 542,30
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
       </div>
     </main>
   )
---
+++ frontend/src/pages/sign-up.tsx
@@ -0,0 +1,127 @@
+import { EyeClosed, Lock, LogIn, Mail, UserRound } from 'lucide-react'
+import { Logo } from '../components/logo'
+
+export function SignUp() {
+  return (
+    <main className="min-h-dvh">
+      <div className="pt-12 max-w-md mx-auto flex flex-col items-center gap-8">
+        <Logo className="h-8" />
+
+        <div className="w-full bg-white border border-gray-200 p-8 rounded-xl flex flex-col gap-8">
+          <div>
+            <h1 className="text-gray-800 text-xl font-bold text-center mb-1">
+              Criar conta
+            </h1>
+
+            <p className="text-center text-gray-600">
+              Comece a controlar suas finanças ainda hoje
+            </p>
+          </div>
+
+          <form action="#" className="space-y-6">
+            <div className="space-y-4">
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="name"
+                  className="text-gray-700 font-medium text-sm"
+                >
+                  Nome completo
+                </label>
+
+                <div className="border border-gray-300 bg-white rounded-lg px-3 flex items-center gap-3">
+                  <div>
+                    <UserRound className="size-4 text-gray-400" />
+                  </div>
+
+                  <input
+                    type="text"
+                    id="name"
+                    className="w-full py-3.5 focus:outline-none leading-2 text-gray-800"
+                    placeholder="Seu nome completo"
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="email"
+                  className="text-gray-700 font-medium text-sm"
+                >
+                  E-mail
+                </label>
+
+                <div className="border border-gray-300 bg-white rounded-lg px-3 flex items-center gap-3">
+                  <div>
+                    <Mail className="size-4 text-gray-400" />
+                  </div>
+
+                  <input
+                    type="email"
+                    id="email"
+                    className="w-full py-3.5 focus:outline-none leading-2 text-gray-800"
+                    placeholder="mail@exemplo.com"
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="password"
+                  className="text-gray-700 font-medium text-sm"
+                >
+                  Senha
+                </label>
+
+                <div className="border border-gray-300 bg-white rounded-lg px-3 flex items-center gap-3">
+                  <div>
+                    <Lock className="size-4 text-gray-400" />
+                  </div>
+
+                  <input
+                    type="password"
+                    id="password"
+                    className="w-full py-3.5 focus:outline-none leading-2 text-gray-800"
+                    placeholder="Digite sua senha"
+                  />
+
+                  <button type="button" className="hover:cursor-pointer">
+                    <EyeClosed className="size-4 text-gray-700" />
+                  </button>
+                </div>
+
+                <span className="text-xs text-gray-500 select-none">
+                  A senha deve ter no mínimo 8 caracteres.
+                </span>
+              </div>
+            </div>
+
+            <button
+              type="button"
+              className="bg-brand-base rounded-lg px-4 py-3 flex items-center justify-center gap-2 font-medium text-white w-full border border-brand-base hover:cursor-pointer hover:bg-brand-dark hover:border-brand-dark transition-colors"
+            >
+              Cadastrar
+            </button>
+
+            <div className="flex items-center gap-3 select-none">
+              <hr className="flex-1 border-gray-300" />
+              <span className="text-sm text-gray-500">ou</span>
+              <hr className="flex-1 border-gray-300" />
+            </div>
+
+            <p className="text-gray-600 text-sm text-center">
+              Ainda não tem uma conta?
+            </p>
+
+            <a
+              href="#"
+              className="bg-white rounded-lg px-4 py-3 flex items-center justify-center gap-2 font-medium text-gray-700 w-full border border-gray-300 hover:cursor-pointer hover:bg-gray-200 transition-colors"
+            >
+              <LogIn className="size-3.5" />
+              Já tem uma conta?
+            </a>
+          </form>
+        </div>
+      </div>
+    </main>
+  )
+}
