5426205c782061bbc68da9366aa0b0a5e582a6f6
Rodrigo Queiroz <rodrigo.queiroz0629@gmail.com>
2026-02-17 19:10:26 -0300

    ﻿feat: configurar servidor GraphQL com Apollo e TypeGraphQL

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,46 @@
+import 'reflect-metadata'
+import { ApolloServer } from '@apollo/server'
+import fastifyApollo, {
+	fastifyApolloDrainPlugin
+} from '@as-integrations/fastify'
 import cors from '@fastify/cors'
 import fastify from 'fastify'
+import { buildSchema } from 'type-graphql'
 import { env } from './env'
+import { CategoryResolver } from './resolvers/category.resolver'
+import { TransactionResolver } from './resolvers/transaction.resolver'
+import { UserResolver } from './resolvers/user.resolver'
 
-const app = fastify()
+async function bootstrap() {
+	const app = fastify()
 
-app.register(cors, {
-	origin: env.CORS_ORIGIN
-})
+	app.register(cors, {
+		origin: env.CORS_ORIGIN
+	})
 
-export { app }
+	const schema = await buildSchema({
+		resolvers: [UserResolver, CategoryResolver, TransactionResolver],
+		validate: false,
+		emitSchemaFile: './schema.graphql'
+	})
+
+	const apollo = new ApolloServer({
+		schema,
+		plugins: [fastifyApolloDrainPlugin(app)]
+	})
+
+	await apollo.start()
+
+	await app.register(fastifyApollo(apollo))
+
+	app.listen(
+		{
+			port: env.APP_PORT
+		},
+		(_err, address) => {
+			console.log(`🚀 Server ready at: ${address}`)
+		}
+	)
+}
+
+bootstrap()
--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import 'reflect-metadata'
-
-import { app } from './app'
-import { env } from './env'
-
-app.listen({ port: env.APP_PORT }, () => {
-	console.log(`Server is running on port ${env.APP_PORT}`)
-})
