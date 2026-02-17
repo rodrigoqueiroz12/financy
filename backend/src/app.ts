5405aabe2df6645178671ad29a27bb1fadd454ea
Rodrigo Queiroz <rodrigo.queiroz0629@gmail.com>
2026-02-17 20:32:48 -0300

    ﻿feat(auth): Adiciona autenticação e registro de usuário

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,7 @@ import cors from '@fastify/cors'
 import fastify from 'fastify'
 import { buildSchema } from 'type-graphql'
 import { env } from './env'
+import { AuthResolver } from './resolvers/auth.resolver'
 import { CategoryResolver } from './resolvers/category.resolver'
 import { TransactionResolver } from './resolvers/transaction.resolver'
 import { UserResolver } from './resolvers/user.resolver'
@@ -19,7 +20,12 @@ async function bootstrap() {
 	})
 
 	const schema = await buildSchema({
-		resolvers: [UserResolver, CategoryResolver, TransactionResolver],
+		resolvers: [
+			AuthResolver,
+			UserResolver,
+			CategoryResolver,
+			TransactionResolver
+		],
 		validate: false,
 		emitSchemaFile: './schema.graphql'
 	})
